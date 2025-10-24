f27ef6eb071835c5007164054a1f93c057586b72
AB_GOWDA <abhishekgowda8090@gmail.com>
2025-10-24 20:20:10 +0530

    Made Propmt Re-Usable and Optimized the Code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate,load_prompt
 import streamlit as Design
 from dotenv import load_dotenv
 import os
@@ -25,32 +25,31 @@ Explanation_style = Design.selectbox( "Select Explanation Style", ["Beginner-Fri
 
 Explanation_length = Design.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
-template = PromptTemplate(
-    template="""
-    Please summarize the research paper titled "{paper_input}" with the following specifications:
-    Explanation Style: {style_input}  
-    Explanation Length: {length_input}  
-    1. Mathematical Details:  
-    - Include relevant mathematical equations if present in the paper.  
-    - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
-    2. Analogies:  
-    - Use relatable analogies to simplify complex ideas.  
-    If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
-    Ensure the summary is clear, accurate, and aligned with the provided style and length.
-    """,
-    input_variables=['paper_input', 'style_input','length_input'],
-    validate_template=True
-)
+# template = PromptTemplate(
+#     template="""
+#     Please summarize the research paper titled "{paper_input}" with the following specifications:
+#     Explanation Style: {style_input}  
+#     Explanation Length: {length_input}  
+#     1. Mathematical Details:  
+#     - Include relevant mathematical equations if present in the paper.  
+#     - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
+#     2. Analogies:  
+#     - Use relatable analogies to simplify complex ideas.  
+#     If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
+#     Ensure the summary is clear, accurate, and aligned with the provided style and length.
+#     """,
+#     input_variables=['paper_input', 'style_input','length_input'],
+#     validate_template=True
+# )
 
-filled_prompt = {
-    'paper_input':Research_paper,
-    'style_input':Explanation_style,
-    'length_input':Explanation_length
-}
+template = load_prompt('template.json')
 
 
 if Design.button('Summarize'):
-    prompt = template.invoke(filled_prompt)
-    result = model.invoke(prompt)
-
+    chain = template | model
+    result = chain.invoke({
+    'paper_input':Research_paper,
+    'style_input':Explanation_style,
+    'length_input':Explanation_length
+    })
     Design.write(result.content)
