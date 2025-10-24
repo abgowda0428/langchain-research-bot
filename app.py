6076c84be527fdea8b39859a68c4f9b937f63b9a
AB_GOWDA <abhishekgowda8090@gmail.com>
2025-10-24 06:33:58 +0530

    Implemented Research Assistant using an open-source model, designed a user-friendly UI for better input, and optimized text-based prompts for improved output.

---
+++ app.py
@@ -0,0 +1,51 @@
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+import streamlit as Design
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+    task="conversational"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+Design.header('Reasearch Tool')
+
+Research_paper = Design.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+Explanation_style = Design.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+Explanation_length = Design.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+template = PromptTemplate(
+    template="""
+    Please summarize the research paper titled "{paper_input}" with the following specifications:
+    Explanation Style: {style_input}  
+    Explanation Length: {length_input}  
+    1. Mathematical Details:  
+    - Include relevant mathematical equations if present in the paper.  
+    - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
+    2. Analogies:  
+    - Use relatable analogies to simplify complex ideas.  
+    If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
+    Ensure the summary is clear, accurate, and aligned with the provided style and length.
+    """,
+    input_variables=['paper_input', 'style_input','length_input'],
+    validate_template=True
+)
+
+filled_prompt = {
+    'paper_input':Research_paper,
+    'style_input':Explanation_style,
+    'length_input':Explanation_length
+}
+
+
+if Design.button('Summarize'):
+    prompt = template.invoke(filled_prompt)
+    result = model.invoke(prompt)
+
+    Design.write(result.content)
