116be9641308f30665cf8fbcf89498d4a57aa251
AB_GOWDA <abhishekgowda8090@gmail.com>
2025-10-28 22:39:20 +0530

    Implemented the Message History

--- chatBot.py
+++ chatBot.py
@@ -1,23 +1,24 @@
 from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from langchain_core.messages import SystemMessage,HumanMessage,AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
 LLm = HuggingFaceEndpoint(
     repo_id='mistralai/Mistral-7B-Instruct-v0.2',
-    task="conversational"
+    task="text-generation"
 )
 
 model = ChatHuggingFace(llm=LLm)
-chat_history = []
+chat_history = [SystemMessage(content="You are an Helpful Agent.")]
 
 while True :
-    user_input = input("Ask Anything...").upper()
-    chat_history.append(user_input)
+    user_input = input("Ask Anything... ").upper()
+    chat_history.append(HumanMessage(content=user_input))
     if user_input == 'EXIT' :
         break
     result = model.invoke(user_input)
-    chat_history.append(result.content)
-    print(result.content)
+    chat_history.append(AIMessage(content=result.content))
+    print('AI :',result.content)
 
 print(chat_history)
